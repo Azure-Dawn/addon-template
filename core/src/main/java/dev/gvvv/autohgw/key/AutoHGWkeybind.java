c00d5a28e9ab50f79b801068033b3d983b316cbb
AzureDawn25270 <121396236+Azure-Dawn@users.noreply.github.com>
2026-04-21 09:13:25 +0200

    Add AutoHGWKeybind class for key binding

---
+++ core/src/main/java/dev/gvvv/autohgw/key/AutoHGWkeybind.java
@@ -0,0 +1,11 @@
+package dev.gvvv.autohgw.key;
+
+import net.labymod.api.client.gui.screen.key.Key;
+import net.labymod.api.client.options.keybinding.KeyBinding;
+
+public class AutoHGWKeybind extends KeyBinding {
+
+    public AutoHGWKeybind() {
+        super("Auto HGW", Key.NONE, "AutoHGW");
+    }
+}
